c0349c4b95ca7240ac9236c181473a63e090f9e3
Gustavo Vinicius Martins Arvelos <gustavoarvelos@Mac-Mini.local>
2021-02-16 20:04:12 -0300

    hotfix : Correções para os testes

--- src/App.js
+++ src/App.js
@@ -1,79 +1,54 @@
-import React, {useState} from "react";
-import api from './services/api';
+import React, { useEffect, useState } from "react";
 
-import "./styles.css";
+import api from './services/api'
 
 function App() {
-  
-  const[repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    async function loadRepositories() {
+      const response = await api.get('repositories')
+
+      setRepositories(response.data)
+    }
+
+    loadRepositories()
+  }, [])
 
   async function handleAddRepository() {
-
-    const title = document.getElementById("title").value;
-
-    if(title.length>0){
-      const response = await api.post("repositories", {
-        title:title
-      })
-        
-      if(response.status>=200){
-        const repository = response.data;
-        document.getElementById("title").value = "";
-        setRepositories([...repositories, repository])
-      } else {
-        alert("Problemas ao adicionar Repositório, por favor tente novamente");
-      }
+    const repository = {
+      title: `GoStack ${Math.random()}`,
+      url: "https://github.com/",
+      techs: ["Node.js", "ReactJS", "React Native"]
     }
+
+    const response = await api.post('repositories', repository)
+
+    setRepositories([...repositories, response.data])
   }
 
   async function handleRemoveRepository(id) {
-    const response = await api.delete("/repositories/" + id);
+    await api.delete(`repositories/${id}`)
 
-    if(response.status>=200){
+    const newRepositories = repositories.filter(repository => repository.id !== id)
 
-      const repositoryIndex = repositories.findIndex( repository => repository.id === id);
-
-      if(repositoryIndex>=0){
-        
-        repositories.splice(repositoryIndex,1); 
-        setRepositories(repositories.filter(repository=>repository.id!== id))
-      }
-    } else {
-      alert("Problemas ao remover Repositório, por favor tente novamente");
-    }
+    setRepositories(newRepositories)
   }
 
-  async function handleListAllRepository(id) {
-    const response = await api.get("/repositories");
-
-    if(response.status>=200){
-      const repositories = response.data;
-      setRepositories(repositories);
-    } else {
-      alert("Problemas ao resquisitar todos os Repositórios, por favor tente novamente");
-    }
-  }
-  
   return (
     <div>
       <ul data-testid="repository-list">
-      <h1>Desafio ReactJS</h1>
-          <ul>
-            {repositories.map(
-              repository => <li key={repository.id}>
-                {repository.title}
-                <button onClick={() => handleRemoveRepository(repository.id)}>
-                  Remover
-                </button>
-              </li>
-              )}
-          </ul>
-          <input placeholder="Titulo" id="title"/>
-          <button onClick={handleAddRepository}>Adicionar</button>
-          <br/>
-          <button onClick={handleListAllRepository}>Listar Todos</button>
-      </ul>      
-    </div>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+          </button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div >
   );
 }
 
